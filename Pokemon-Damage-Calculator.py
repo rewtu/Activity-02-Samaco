00105b26ca1bb26e0f707e1a4f4f7a900dd7d94e
rewtu <rewtusamaco@gmail.com>
2022-04-13 16:48:13 +0800

    Final Work

--- Pokemon-Damage-Calculator.py
+++ Pokemon-Damage-Calculator.py
@@ -1,3 +1,4 @@
+import random
 level = 90
 A = 205
 D = 188
@@ -6,14 +7,19 @@ Targets = 1
 Weather = 1
 Badge = 1
 Critical = 1
-
+randamage = 0
 STAB = 1.5
 Type = 0.5
 Burn = 1
 other = 1
 
-Modifier = Targets * Weather * Badge * Critical * STAB * Type * Burn * other
-Modifier
+rand = random.randint(1,2)
+if rand == 1:
+        randamage = 0.85
+elif rand == 2:
+        randamage = 1.0
+Modifier = Targets * Weather * Badge * rand * Critical * STAB * Type * Burn * other
 Damage = (((level * 2 / 5 + 2 ) * Power * A / D)/ 50 + 2 ) * Modifier
-
-Damage
+#dam =int(Damage)
+print("Charizard's attack damage:")
+print(Damage)
